c2084d5911f5de643761365a6f32ba67b7496d7d
angiekierra <angie.kierra@gmail.com>
2024-03-02 12:09:51 +0700

    feat: new strategy not fixed

--- tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/newBot.py
+++ tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/newBot.py
@@ -35,7 +35,7 @@ class NewBot(BaseLogic):
                 teleporter.append(item)
         return teleporter
     
-    def find_nearest_teleporter(self, board_bot: GameObject, teleporters):
+    def find_nearest_teleporter(self, board_bot: GameObject, teleporters : list[GameObject]):
         nearest_teleporter = None
         min_distance = float('inf')
 
@@ -44,30 +44,44 @@ class NewBot(BaseLogic):
             if (distance < min_distance):
                 min_distance = distance
                 nearest_teleporter = teleport
-        return nearest_teleporter
+        return nearest_teleporter,min_distance
     
-    def get_nearest_diamond(self,board_bot: GameObject, board : Board) :
+    def get_nearest_diamond(self,teleport: GameObject, board : Board) :
         nearest_diamond = None  
         min_distance = float('inf')
 
         for diamond in board.diamonds: 
-            distance = (abs(board_bot.position.x - diamond.position.x)) + (abs(board_bot.position.y - diamond.position.y))
+            distance = (abs(teleport.position.x - diamond.position.x)) + (abs(teleport.position.y - diamond.position.y))
             if distance < min_distance :
                 min_distance = distance
                 nearest_diamond = diamond
-        return nearest_diamond
+        return nearest_diamond,min_distance
     
-
-    # def teleport_ratio()
     def calculate_distance(self, point1: Position, point2: Position) -> Optional[int]:
         return abs(point1.x - point2.x) + abs(point1.y - point2.y)
+
+    def teleport_ratio(self,board_bot: GameObject, board : Board, teleporter : list[GameObject]):
+        nearest_teleporter = self.find_nearest_teleporter(board_bot,teleporter)[0]
+        nearest_teleporter_position = nearest_teleporter.position
+        pair = None
+        for teleport in teleporter:
+            if teleport.id != nearest_teleporter.id:
+                pair = teleport
+
+        nearest_diamond = self.get_nearest_diamond(pair,board)
+        distance_to_teleport = self.calculate_distance(board_bot.position,nearest_teleporter.position)
+        distance_to_diamond = nearest_diamond[1]
+        total_distance = distance_to_teleport+distance_to_diamond
+        return (total_distance/nearest_diamond[0].properties.points),nearest_teleporter_position
+
     
     def next_move(self, board_bot: GameObject, board: Board):
         props = board_bot.properties
         timeleft = board_bot.properties.milliseconds_left
         base = board_bot.properties.base
         distance_base = self.calculate_distance(board_bot.position,base)
-        
+        teleporters = self.get_teleporters(board)
+
         if timeleft <= (distance_base*1000):
             # Move to base
             self.goal_position = base
@@ -77,15 +91,18 @@ class NewBot(BaseLogic):
             else:
                 if(props.diamonds == 4):
                     best_diamond = self.best_diamond_ratio(board_bot,board)
-                    distance_diamond = self.calculate_distance(board_bot.position,best_diamond)
-                    if (distance_base < distance_diamond[0]):
+                    distance_diamond = self.calculate_distance(board_bot.position,best_diamond[0])
+                    if (distance_base < distance_diamond):
                         self.goal_position = base
                     else:
                         self.goal_position = best_diamond[0]
                 else:
+                    ratio_teleport = self.teleport_ratio(board_bot,board,teleporters)
                     best_diamond = self.best_diamond_ratio(board_bot,board)
-                    self.goal_position = best_diamond[0]
-
+                    if (ratio_teleport[0] > best_diamond[1] ) :
+                        self.goal_position = ratio_teleport[1]
+                    else:
+                        self.goal_position = best_diamond[0]
 
 
         current_position = board_bot.position

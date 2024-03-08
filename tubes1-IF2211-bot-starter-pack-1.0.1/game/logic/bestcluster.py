4b04a20e4fa98999119a5762f9086a33597c2811
Neo Koda <mneocicerok@gmail.com>
2024-03-08 18:49:54 +0700

    chore : tidy up code

--- tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/bestcluster.py
+++ tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/bestcluster.py
@@ -5,6 +5,7 @@ from game.logic.base import BaseLogic
 from game.models import GameObject, Board, Position
 from ..util import clamp, position_equals
 
+# Modified get direction function
 def get_direction(current_x, current_y, dest_x, dest_y, h_priority):
     delta_x = clamp(dest_x - current_x, -1, 1)
     delta_y = clamp(dest_y - current_y, -1, 1)
@@ -16,6 +17,7 @@ def get_direction(current_x, current_y, dest_x, dest_y, h_priority):
             delta_x = 0
     return (delta_x, delta_y)
 
+# Distance counting functions
 def distance(p1: Position, p2: Position):
     return abs(p1.x - p2.x) + abs(p1.y - p2.y)
 
@@ -25,6 +27,7 @@ def distance_tp(src: Position, dest: Position, close_tp: Position, far_tp: Posit
 def min_distance(src: Position, dest: Position, close_tp: Position, far_tp: Position):
     return min(distance(src, dest), distance_tp(src, dest, close_tp, far_tp))
 
+# Teleporter functions
 def teleporter_positions(board: Board):
     teleporters = []
     for game_object in board.game_objects:
@@ -36,6 +39,7 @@ def blocked_by_teleporter(position: Position, board: Board):
     teleporters = teleporter_positions(board)
     return (position_equals(teleporters[0], position) or position_equals(teleporters[1], position))
 
+# Red button finder
 def red_button(board_bot: GameObject, board: Board, close_tp: Position, far_tp: Position):
     for game_object in board.game_objects:
         if (game_object.type == "DiamondButtonGameObject"):
@@ -47,6 +51,7 @@ def red_button(board_bot: GameObject, board: Board, close_tp: Position, far_tp: 
 
     return red_button if ((total_distance_normal <= total_distance_tp) or position_equals(board_bot.position, close_tp)) else close_tp 
 
+# Cluster functions
 def within_cluster(p1: Position, p2: Position):
     return (abs(p1.x - p2.x) < 3 and abs(p1.y - p2.y) < 3)
 
@@ -57,6 +62,7 @@ def count_diamond_cluster(diamond: GameObject, diamonds: List[GameObject]):
             sum += diamond2.properties.points
     return sum
 
+# Find best cluster
 def best_cluster(board_bot: GameObject, board: Board, close_tp: Position, far_tp: Position):
     diamonds = board.diamonds
     props = board_bot.properties
@@ -64,8 +70,6 @@ def best_cluster(board_bot: GameObject, board: Board, close_tp: Position, far_tp
     best_cluster = diamonds[0]
     min_distance_points_ratio = 10000
     through_tp = False
-    chosen_distance = 100000
-    max_points = 0
 
     for diamond in diamonds:
         if (props.diamonds == 4 and diamond.properties.points == 2):
@@ -80,16 +84,11 @@ def best_cluster(board_bot: GameObject, board: Board, close_tp: Position, far_tp
         if ((total_distance / min(total_diamonds, props.inventory_size - props.diamonds)) < min_distance_points_ratio):
             min_distance_points_ratio = (total_distance / min(total_diamonds, props.inventory_size - props.diamonds))
             best_cluster = diamond
-            max_points = min(total_diamonds, props.inventory_size - props.diamonds)
-            chosen_distance = total_distance
             through_tp = True if (total_distance == total_distance_tp) else False
 
-    print(f"Current position: {board_bot.position.x} {board_bot.position.y}")
-    print(f"Best cluster: {best_cluster.position.x} {best_cluster.position.y}")
-    print(f"Distance: {chosen_distance}")
-    print(f"Points: {max_points}")
     return close_tp if (through_tp and not(position_equals(board_bot.position, close_tp))) else best_cluster.position
 
+# Find best cluster while returning to base
 def best_cluster_base(board_bot: GameObject, board: Board, close_tp: Position, far_tp: Position):
     diamonds = board.diamonds
     props = board_bot.properties
@@ -123,6 +122,7 @@ def best_cluster_base(board_bot: GameObject, board: Board, close_tp: Position, f
     print(f"Points: {max_points}")
     return close_tp if (through_tp and not(position_equals(board_bot.position, close_tp))) else best_cluster.position
 
+# Direct the bot to base
 def to_base(board_bot: GameObject, board: Board, close_tp: Position, far_tp: Position):
     base = board_bot.properties.base
 
@@ -142,8 +142,6 @@ class BestClusterLogic(BaseLogic):
         props = board_bot.properties
         current_position = board_bot.position
         base = board_bot.properties.base
-
-        print(props.milliseconds_left)
         
         teleporters = teleporter_positions(board)
         close_tp = teleporters[0] if (distance(board_bot.position, teleporters[0]) < distance(board_bot.position, teleporters[1])) else teleporters[1]
@@ -152,10 +150,9 @@ class BestClusterLogic(BaseLogic):
         distance_to_base = min(distance(current_position, base), distance_tp(current_position, base, close_tp, far_tp))
         d_red_button = distance(red_button(board_bot, board, close_tp, far_tp), current_position)
 
-        if (props.diamonds == 5):
+        if (props.diamonds == 5): # Go to base
             self.goal_position = to_base(board_bot, board, close_tp, far_tp)
-            print(f"To base chosen (full)")
-        elif (props.diamonds >= 3):
+        elif (props.diamonds >= 3): # Find best cluster while returning
             cluster = best_cluster_base(board_bot, board, close_tp, far_tp)
             red = red_button(board_bot, board, close_tp, far_tp)
 
@@ -164,31 +161,25 @@ class BestClusterLogic(BaseLogic):
             d_red_button_base = min_distance(red, base, close_tp, far_tp)
 
             if (d_red_button < d_current_cluster and (d_red_button + d_red_button_base) < (distance_to_base + 5) and not(position_equals(red, close_tp))):
-                self.goal_position = red
-                print(f"Red button chosen")
+                self.goal_position = red # If red button is nearest
             elif (d_current_cluster + d_cluster_base) < distance_to_base + 5 and not(position_equals(cluster, close_tp)):
-                self.goal_position = cluster
-                print(f"Best cluster chosen (returning)")
+                self.goal_position = cluster # If taking a detour to the cluster doesnt take too long
             else:
+                # If everything else is too far
                 self.goal_position = to_base(board_bot, board, close_tp, far_tp)
-                print(f"To base chosen (next item too far)")
         else:
+            # Search for best cluster or nearest red button
             d_best_cluster = distance(best_cluster(board_bot, board, close_tp, far_tp), current_position)
 
             if (d_red_button < d_best_cluster and not(position_equals(red_button(board_bot, board, close_tp, far_tp), close_tp))):
                 self.goal_position = red_button(board_bot, board, close_tp, far_tp)
-                print(f"Red button chosen")
             else:
                 self.goal_position = best_cluster(board_bot, board, close_tp, far_tp)
-                print(f"Best cluster chosen")
 
-        if ((props.milliseconds_left / 1000) - 1.5 <= distance_to_base):
+        if ((props.milliseconds_left / 1000) - 1.5 <= distance_to_base): # Return to base in time
             self.goal_position = to_base(board_bot, board, close_tp, far_tp)
-            print(f"Distance: {distance(current_position, self.goal_position)}")
-            print(f"To base chosen (time)")
 
         if self.goal_position:
-            print(f"Goal position {self.goal_position.x} {self.goal_position.y}")
             # We are aiming for a specific position, calculate delta
             delta_x, delta_y = get_direction(
                 current_position.x,
@@ -199,23 +190,23 @@ class BestClusterLogic(BaseLogic):
             )
             
             new_position = Position(x=(current_position.x + delta_x), y=(current_position.y + delta_y))
-            # check if destination isnt a teleporter, but blocked by a teleporter
+            # Check if destination isnt a teleporter, but blocked by a teleporter
             if (blocked_by_teleporter(new_position, board) and not(position_equals(self.goal_position, close_tp))):
                 print(f"Blocked by teleporter")
-                if (delta_x != 0): # if initially going vertically, move horizontally
+                if (delta_x != 0): # If initially going vertically, move horizontally
                     delta_x = 0
                     self.h_priority = True
                     if (board.is_valid_move(current_position, delta_x, 1)): 
                         delta_y = 1
                     else:
-                        delta_y = -1 # go down if cant go up (at the top of the board)
+                        delta_y = -1 # Go down if unable to go up (at the top of the board)
                 else:
                     delta_y = 0
                     self.h_priority = False
                     if (board.is_valid_move(current_position, 1, delta_y)):
                         delta_x = 1
                     else:
-                        delta_x = -1 # go left if cant go right (at the right edge of the board)
+                        delta_x = -1 # Go left if unable to go right (at the right edge of the board)
         else:
             # Roam around
             delta = self.directions[self.current_direction]
